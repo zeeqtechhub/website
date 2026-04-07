a5f275448c17542710ad40754e84ac0342dc4982
olanusi-code <dele.olanusi@errandpay.com>
2026-04-07 10:08:41 +0100

    Current menu

--- function.php
+++ function.php
@@ -559,12 +559,9 @@ function slugify($text) {
  * @param string $cssClass
  * @return string|null
  */
-function current_menu($page = null, $cssClass = "active") {
-    $path = trim(parse_url(getFullUrl(), PHP_URL_PATH), "/");
-    $page = (!$page || $page == "index" || $page == "home") ? "" : $page;
-
-    if($path === $page) return $cssClass;
-    return null;
+function current_menu($page = "home", $cssClass = "active") {
+    // Compare against the current page
+    return ($page === getCurrentPage()) ? $cssClass : null;
 }
 
 /**
